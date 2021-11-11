9873fa2ed835acdfab5b8963767ee1c1fa13b2c8
Md Azizul <banglatechmaster1@gmail.com>
2021-11-12 02:03:16 +0600

    Make Secure

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const serviceAccount = require("./ceci-drone-firebase-adminsdk.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
